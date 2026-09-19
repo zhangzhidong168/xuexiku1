661c5be3627f18c69fb4d2c68f6981f270b217c7
张志栋 <914070006@qq.com>
2026-09-19 20:38:10 +0800

    Day18:第一个VS Code Python文件

--- day18.py
+++ day18.py
@@ -8,3 +8,16 @@ birth = 1978
 age = year - birth
 print("我今年" + str(age) + "岁")
 
+
+# 按键盘ctrl + · 1 字的左边
+# 打开终端
+# 输入python + 文件名  回车输出
+
+# 在sh 窗口 输入
+# cd ~/文件夹名 (如：git_practice)
+# code .
+# git add .
+# git commit -m "说明：（如：Day18：第一个VS Code Python文件）"
+# git push
+# git log
+
